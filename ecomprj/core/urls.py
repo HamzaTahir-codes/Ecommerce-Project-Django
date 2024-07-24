576ba91b0c603ade9145bdb726758bd247a848cb
HamzaTahir-codes <m.hamza.codes@gmail.com>
2024-07-25 02:19:04 +0500

    Added Core Models & Views

--- ecomprj/core/urls.py
+++ ecomprj/core/urls.py
@@ -4,5 +4,7 @@ from . import views
 app_name = 'core'
 
 urlpatterns = [
-    path('', views.index, name="index")
+    path('', views.index, name="index"),
+    path("products/", views.product_list, name="product-list"),
+    path("category/", views.category_list, name="category-list"),
     ]
--- ecomprj/core/views.py
+++ ecomprj/core/views.py
@@ -1,5 +1,26 @@
 from django.shortcuts import render
-
+from .models import Product, Category, Vendor, CartOrder, CartOrderItems, WishList, ProductReview, Address, ProductImages
+from django.db.models import Count
 
 def index(request):
-    return render(request, 'core/index.html')
+    # product = Product.objects.all().order_by("-id")
+    product = Product.objects.filter(product_status = "published", featured = True)
+    context = {
+        "product" : product,
+    }
+    return render(request, 'core/index.html', context)
+
+def product_list(request):
+    product = Product.objects.filter(product_status = "published")
+    context = {
+        "products" : product,
+    }
+    return render(request, 'core/product-list.html', context)
+
+def category_list(request):
+    categories = Category.objects.all()
+    # categories = Category.objects.all().annotate(product_count = Count("products"))
+    context = {
+        "categories" : categories,
+    }
+    return render(request, "core/category-list.html", context)
